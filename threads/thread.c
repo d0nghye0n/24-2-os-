922003ed534518bd1dc5bf00cbfa5c4725bda193
d0nghye0n <c20537@kma.ac.kr>
2024-12-29 22:05:55 +0900

    Update thread.c

--- threads/thread.c
+++ threads/thread.c
@@ -196,6 +196,53 @@ thread_print_stats (void)
           idle_ticks, kernel_ticks, user_ticks);
 }
 
+void
+donate_priority(void)
+{
+   int depth;
+   stuct thread *cur = thread_current();
+
+   for(depth = 0; depth < 0; depth++)
+   {
+      if(!cur -> wait_on_lock) break;
+         struct thread *holder = cur -> wait_on_lock -> holder;
+         holder -> priority = cur -> priority;
+         cur = holder;
+   }
+}
+
+void
+remove_with_lock(struct lcok *lock)
+{
+   struct list_elem *e;
+   struct thread *cur = thread_current();
+
+   for(e = list_begin(&cur -> donations); e != list_end(&cur -> donations); e = list_next(e))
+   {
+      struct thread *t = list_entry(e, struct thread, donation_elem);
+      if(t -> wait_on_lock == lock)
+         list_remove(&t -> donation_elem);
+   }
+}
+
+void
+refresh_priority(void)
+{
+   struct thread *cur = thread_current();
+   cur -> priority = cur -> init_priority;
+
+   if(!list_empty(&cur -> donations))
+   {
+      list_sort(&cur -> donations, thread_compare_donate_priority, 0);
+
+      struct thread *front = list_entry(list_front(&cur -> donations), struct thread, donation_elem);
+      if(front -> priority > cur -> priority)
+         cur -> priority = front -> priority;
+   }
+}
+
+
+
 /* Creates a new kernel thread named NAME with the given initial
    PRIORITY, which executes FUNCTION passing AUX as the argument,
    and adds it to the ready queue.  Returns the thread identifier
@@ -387,7 +434,9 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  thread_current ()->priority = new_priority;
+  thread_current () -> init_priority = new_priority;
+
+  refresh_priority();
   thread_test_preemption ();
 }
 
@@ -641,6 +690,12 @@ thread_compare_priority(struct list_elem *l, struct list_elem *s, void *aux UNUS
    // 우선순위 비교
    return list_entry(l, struct thread, elem) -> priority
       > list_entry(s, struct thread, elem) -> priority;
+}
+
+bool
+thread_compare_donate_priority(const struct list_elem *l, const struct list_elem *s, void *aux UNUSED)
+{
+   return list_entry(l, struct thread, donation_elem) -> priority > list_entry(s, sturct thread, donation_elem) -> priority;
 }
 
 /* Offset of `stack' member within `struct thread'.
